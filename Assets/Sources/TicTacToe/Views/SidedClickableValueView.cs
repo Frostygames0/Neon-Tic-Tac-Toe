f1baf9825a39520b073b32bd94a53e12126b4abc
Ваня <vanya.alekseev6@gmail.com>
2023-04-27 20:30:34 +0300

    Now SidedClickableValueView displays data correctly

--- Assets/Sources/TicTacToe/Views/SidedClickableValueView.cs
+++ Assets/Sources/TicTacToe/Views/SidedClickableValueView.cs
@@ -1,14 +1,19 @@
-﻿namespace TicTacToe.Views
+﻿using System;
+
+namespace TicTacToe.Views
 {
     public class SidedClickableValueView : ClickableValueView<GameSide>
     {
         protected override string UpdateValue(GameSide value)
         {
-            if (value == GameSide.Indeterminate)
-                return "";
-            
-            var convertedSign = value.ToString("G");
-            return convertedSign;
+            return value switch
+            {
+                GameSide.Circle => "O",
+                GameSide.Cross => "X",
+                GameSide.Indeterminate => "",
+                _ => throw new ArgumentOutOfRangeException(nameof(value), value,
+                    "Somehow you've managed to go out of range for GameSide, an enum...")
+            };
         }
     }
 }
